d3025e27b7737fc989a3e0dc68064aeee21b8fb9
Atishay-j-a-in <shepherdk450@gmail.com>
2026-06-19 15:42:21 +0530

    verify meta tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
+import type { Metadata } from "next";
 import { Stage } from "./_components/voyage/Stage";
+
+export const metadata: Metadata = {
+  other: {
+    "google-site-verification": "tYZTuGSt007FnLrQkrxF3DnAjILwFNHzV4r7oRwpkw8",
+  },
+};
 
 export default function Home() {
   return <Stage />;
