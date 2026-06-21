15e7c6e0d0c74b60ac8c174721fddba5d517e665
Atishay-j-a-in <shepherdk450@gmail.com>
2026-06-21 15:42:30 +0530

    refactor: restore serve function and API methods in route.ts

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,12 +1,7 @@
-// import { serve } from "inngest/next";
-// import { inngest } from "@/inngest/client";
-// import { gmailInitialSync, gmailContinuousSync, gmailChanged, dailyEmailSummary } from "@/inngest/functions";
-// export const { GET, POST, PUT } = serve({
-//   client: inngest,
-//   functions: [gmailInitialSync, gmailContinuousSync, gmailChanged, dailyEmailSummary],
-// });
-export async function GET() {
-  return Response.json({
-    works: true,
-  });
-}
+import { serve } from "inngest/next";
+import { inngest } from "@/inngest/client";
+import { gmailInitialSync, gmailContinuousSync, gmailChanged, dailyEmailSummary } from "@/inngest/functions";
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [gmailInitialSync, gmailContinuousSync, gmailChanged, dailyEmailSummary],
+});
